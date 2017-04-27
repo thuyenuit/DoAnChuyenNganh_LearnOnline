6c5e2bfeb5320f2a0d1591f26323fad53a536b7f
ThuyenBu <nhatthu100@gmail.com>
2017-04-27 09:51:13 +0700

    Khởi tạo constructor của ExamController

--- LearnOnline.Web/Api/ExamController.cs
+++ LearnOnline.Web/Api/ExamController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearnOnline.Service.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,23 @@ namespace LearnOnline.Web.Api
 {
     public class ExamController : ApiController
     {
+        #region Initialize
+        private IUserService _userService;
+        private IUserGroupService _userGroupService;
+        private IDistrictService _districtService;
+        private IProvinceService _provinceService;
+
+        public ExamController(IUserService userService,
+                            IUserGroupService userGroupService,
+                            IDistrictService districtService,
+                            IProvinceService provinceService)
+            : base()
+        {
+            this._userService = userService;
+            this._userGroupService = userGroupService;
+            this._districtService = districtService;
+            this._provinceService = provinceService;
+        }
+        #endregion    
     }
 }
